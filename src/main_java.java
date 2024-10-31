5ebfa53fcd7eb5aeeff3bfe449a4886024cc450a
Yunusov Jamoliddin <legion26042010@gmail.com>
2024-10-31 13:10:55 +0300

    new

--- src/main_java.java
+++ src/main_java.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class main_java {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.println(a*4);
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int r1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        int r2 = scanner.nextInt();
+        double r = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        if (r1 + r2 >= r && r1 + r >= r2 && r + r2 >= r1) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
